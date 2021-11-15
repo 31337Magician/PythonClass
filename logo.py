cb2869146dfb68e2c11383f3e2c07d13932ba9c6
31337Magician <94198391+31337Magician@users.noreply.github.com>
2021-11-15 08:14:38 -0500

    Python Homework
    Build a program to print an Ascii logo.

---
+++ logo.py
@@ -0,0 +1,54 @@
+# Python Homework
+print ('''/
+# Input Art
+
+                                                                                                    
+                                                                                                    
+                                    ..``::::-:///:.`                                                
+                              `.--:o///y/+o+y/-:.-://:-.`                                           
+                          ``.:o///h.y:sy-dhdo`+.``...-:://:-.                                       
+                         /+//y:sdhd.dsh/smmy`--      ``...-:://.                                    
+                        `h.yod/smdy:mmh-mmd...           ```-`.y                                    
+                         y.hod+omm+omm/smm-`-    -.`        : +/                                    
+                         +/smm+omm:ymd.mm+ :    /myyo`     -`:s                                     
+                         .y:mmo+mm.dm/oms :`   -ds om.    ...y`                                     
+                          y.dms/md.md.mh`-`   `hh`:m+    `- y.                                      
+                          +/smy:ms/m+od..-    yd..dy     : o:                                       
+                          .y:my:m+od.d:`:    /dy/hh`    : /+                                        
+                           y.dh-m:hsoo :      `-/:`    -`-s                                         
+                           //sd.mh+:+++:`             .-`y`                                         
+                           .y:m`do/``..:+/-`         `- s-                                          
+                            y.d.y`:o:.-` `:s/.```   .: +/                                           
+                            //s-s+//yo.-.  `o+:-...`+-:s+                                           
+                            .y:s++osyhy-..  -yhs+/://:s`s`                                          
+                             :+o++y+o/:s:``   `  `.-:-` o.                                          
+                               ::+o`+:-/h.              ++                                          
+                                  --.   +o`             `s                                          
+                                         oy-             :/                                         
+                                         `s+`            -do:                                       
+                                 `-:/+oooooyh`       `:odydmmhyoo+/-.                               
+                            `:+oo+/:.` `..--ss`     ::syymmmmmmo::::+ooo/.                          
+                           /y/.` .:+syddddddmmdo-  `.yydmmmmmmmmdhys+-``:ss.                        
+                          .m.   `ydmmmmmmmmmmmmmmho-hdmmmmmmmmmmmmhyss   `do                        
+                           os-   `:+syddmmmmmmdddhhhhhhhhhhhhhyssoo+-` `-oh-                        
+                            -+o+/-.```..-://++ooooooooo++//::------:/oyys/`                         
+                              `.-/++yysso+++///::::::::////+osyhddys+/-.                            
+                                    yyshhdddmmmmmmmmmmmmmmmmmmmmmm.                                 
+                                    y/` `.:/+osyyhhhddddddddmmmmmh-                                 
+                                    h/-         ``....------mmmmms:                                 
+                                    d+-                     mmmmmo/                                 
+                                   `m+-    ``               dmmmmoo                                 
+                                   -do. ``..:/+ossssssoossyhmmmmm+y                                 
+                                   /ys              ``````  hmmmm+d                                 
+                                   sos                      ymmmmyh`                                
+                                   d/+                      ommmmds:                                
+                                  .m/.                      +mmmmmss                                
+                                  +y:                       :mmmmmhd                                
+                                  y/`                       .mmmmmmm-                               
+                                  os.                        dmmmddh.                               
+                                   /s+.`  ``                 yddyyo.                                
+                                    `-+oo+/:::::::---..```.-:os+/.                                  
+                                        `-:/+++ooooooooo+++/-.`                                     
+                                                                                                     '
+''')
+#End
